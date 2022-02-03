252c168df0507042af25ebee8638d27722c4f921
Tikaram Mardi <tikarammardi007@gmail.com>
2022-02-03 13:03:26 +0530

    port

--- app.js
+++ app.js
@@ -97,5 +97,6 @@ const loadPincodes = () => {
 	}
 };
 
-//app.listen(3000, () => console.log(`Listening on: 3000`));
+const PORT = process.env.PORT || 3000;
+//app.listen(PORT, () => console.log(`Listening on: ${PORT}`));
 module.exports.handler = serverless(app);
