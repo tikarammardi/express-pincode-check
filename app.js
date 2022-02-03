fa50324c45eceb86c6c33eb82dcefe18cd0e4608
Tikaram Mardi <tikarammardi007@gmail.com>
2022-02-03 19:19:42 +0530

    final commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const fs = require('fs');
 const process = require('process');
 const cors = require('cors');
+
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -69,14 +70,31 @@ app.post('/upload', (req, res) => {
 	}
 });
 
+app.get('/pincodes', (req, res) => {
+	try {
+		const pincodes = loadPincodes();
+		console.log('pincodes loaded', pincodes);
+
+		return res.status(200).json({
+			pincodes
+		});
+	} catch (error) {
+		console.error('error is', error);
+		return res.status(500).json({
+			error: true,
+			message: error.message
+		});
+	}
+});
+
 const savePincodes = (pincode) => {
 	try {
-		console.log('Starting directory: savePincodes', process.cwd());
+		console.log('Starting directory: savePincodes', process.cwd(), pincode);
 		process.chdir('/tmp');
 		console.log('New directory: ', process.cwd());
 		const dataJSON = JSON.stringify(pincode);
 		console.log('dataJSON');
-		fs.writeFileSync(`${process.cwd()}/pincodes.json`, dataJSON);
+		fs.writeFileSync(`pincodes.json`, dataJSON);
 	} catch (error) {
 		console.log('error in saving pincodes savePincodes', error);
 	}
@@ -87,7 +105,7 @@ const loadPincodes = () => {
 		console.log('Starting directory: loadPincodes', process.cwd());
 		process.chdir('/tmp');
 		console.log('New directory: ', process.cwd());
-		const dataBuffer = fs.readFileSync(`${process.cwd()}/pincodes.json`);
+		const dataBuffer = fs.readFileSync(`pincodes.json`);
 		const dataJSON = dataBuffer.toString();
 		console.log('dataJson', dataJSON);
 		return JSON.parse(dataJSON);
@@ -97,6 +115,6 @@ const loadPincodes = () => {
 	}
 };
 
-const PORT = process.env.PORT || 3000;
+//const PORT = process.env.PORT || 3000;
 //app.listen(PORT, () => console.log(`Listening on: ${PORT}`));
 module.exports.handler = serverless(app);
